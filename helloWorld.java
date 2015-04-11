ce7765e08ab5b3f65eacb9988e9d2383ee16e55b
Juan Antonio Guerra Montenegro <evanblackhill@gmail.com>
2015-04-11 23:28:04 +0100

    Delete helloWorld.java
    Old class deletion.

--- helloWorld.java
+++
@@ -1,7 +0,0 @@
-package javabasics;
- 
-public class helloWorld {
-    public static void printHelloWorld (String word){
-        System.out.println(word);                       // Word Variable Print.
-    }
-}
