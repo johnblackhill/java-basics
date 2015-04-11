5e253f9e50d8039a82c7c1c87d7e7e46331c132a
Juan Antonio Guerra Montenegro <evanblackhill@gmail.com>
2015-04-11 23:23:07 +0100

    "Strings" Class code tidy up.
    - Just forgot to add a few comments about operations and to change a
    variable name to something more logical ("greeting" is now
    "displayName").
    - Added end dots to the Numerical Strings display.
    - Indented "Strings" class comments.

--- javabasics/strings.java
+++ javabasics/strings.java
@@ -3,22 +3,22 @@ package javabasics;
 public class Strings {
     public static void strPrint(){
         // String Variables.
-        String text = "This is a name:";         // "text" refers to a previously declared String class.
-                                                 // "String" is not a basic type, so it starts with Capital Letters.
-        String blank = " ";
-        String name = "Peter Griffin";
-        String greeting = text + blank + name;
+        String text = "This is a name:";            // "text" refers to a previously declared String class.
+                                                    // "String" is not a basic type, so it starts with Capital Letters.
+        String blank = " ";                         // Blank space String.
+        String name = "Peter Griffin";              // Name String
+        String displayName = text + blank + name;   // String made of String operations.
 
         // Numerical Variables.
         short s_int = 22;
         double d_int = 22.2;
 
         // Printing Lines.
-        System.out.println(greeting);
-        System.out.println("This is " + "a hardcoded " + "string operation");
-        System.out.println("This is an Integer: " + s_int);
-        System.out.println("This is a Double: " + d_int);
+        System.out.println(displayName);
+        System.out.println("This is " + "a hardcoded " + "string operation.");  // Hardcoded String concatenation.
+        System.out.println("This is an Integer: " + s_int + ".");               // Concatenating Strings & Vars #1
+        System.out.println("This is a Double: " + d_int + ".");                 // Concatenating Strings & Vars #2
 
-        System.out.println();                               // Separation Line
+        System.out.println();                                                   // Separation Line
     }
 }
